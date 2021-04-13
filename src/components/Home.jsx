efc887edb27ab0d451eb32de5b73212ce04dc909
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-04-12 22:25:57 -0400

    added base react files

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <img
+                src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png"
+                alt="bank"
+            />
+            <h1>Bank of React</h1>
+        </div>
+    );
+}
+
+export default Home;
