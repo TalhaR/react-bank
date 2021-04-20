a44b9d2a3326b500c928ecc56bbd69f95b012c92
Darren Liang <liangdarren2@gmail.com>
2021-04-20 11:17:41 -0400

    credits page functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home'
 import DebitsPage from './components/DebitsPage';
+import CreditsPage from './components/CreditsPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/credits" component={CreditsPage} />
         <Route path="/debits" component={DebitsPage} />
         <Route path="/" component={Home} />
       </Switch>
